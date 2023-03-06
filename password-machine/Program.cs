f6e32e6a1a96c8ec28682978d6b2b702e873adc9
Tim Doren <dorentim@gmail.com>
2023-03-06 14:46:16 -0500

    added a working shuffle algorithm

--- password-machine/Program.cs
+++ password-machine/Program.cs
@@ -1,37 +1,37 @@
 ﻿const string lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const string upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const string specialChar = "!@#$%^&*()_-><{}[]?";
+const string specialChar = "!@#$%^&*_-><?";
 Random random = new Random();
 string specialPass = "";
 string lowerPass = "";
 string upperPass = "";
-for (int i = 0; i < 4; i++ )
+for (int i = 0; i < 5; i++ )
 {
     int index = random.Next(lowerCase.Length);
     char randomChar = lowerCase[index];
-    lowerPass = Char.ToString(randomChar);
+    lowerPass += Char.ToString(randomChar);
 }
-for (int i = 0; i < 4; i++)
+for (int i = 0; i < 5; i++)
 {
     int index = random.Next(upperCase.Length);
     char randomChar = upperCase[index];
-    upperPass = Char.ToString(randomChar);
-  //  Console.Write(upperPass);
+    upperPass += Char.ToString(randomChar);
 }
-for (int i = 0; i < 4; i++)
+for (int i = 0; i < 5; i++)
 {
     int index = random.Next(specialChar.Length);
     char randomChar = specialChar[index];
-    specialPass = Char.ToString(randomChar);
-  //  Console.Write(specialPass);
+    specialPass += Char.ToString(randomChar);
 }
-Console.WriteLine(lowerPass + upperPass + specialPass);
-//void Shuffle()
-//{
-//    password = password + password1 + password2;
-//    string strPassword = password;
-//    int index = random.Next(password.Length);
-//    char randomChar = password[index];
-//    password = Char.ToString(randomChar);
-//}
-//Console.WriteLine(password);
+var password1 = upperPass + lowerPass + specialPass;
+Console.WriteLine(password1);
+char[] passwordChars = password1.ToCharArray();
+for (int i = passwordChars.Length - 1; i > 0; i--)
+{
+    int j = random.Next(i + 1);
+    char temp = passwordChars[i];
+    passwordChars[i] = passwordChars[j];
+    passwordChars[j] = temp;
+}
+password1 = new string(passwordChars);
+Console.WriteLine(password1);
